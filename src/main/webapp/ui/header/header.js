e1b31c2c3a4cb58614951eeaadc2abd56c9235ab
54data <ehdvkf02@naver.com>
2024-08-15 23:22:11 +0900

    Feat: 검색 이벤트시 파라미터 전달 관련 함수 구현

--- src/main/webapp/ui/header/header.js
+++ src/main/webapp/ui/header/header.js
@@ -7,12 +7,15 @@ function categoryOpen(x) {
 function searchTypeText() {
     const placeholderText = "✨딥클렌징으로 모공 속 피지, 싹- 제거!✨";
     if (textIndex < placeholderText.length) {
-        $(".header-search-input").attr("placeholder", placeholderText.substring(0, textIndex + 1));
+        $(".header-search-input").attr(
+            "placeholder",
+            placeholderText.substring(0, textIndex + 1)
+        );
         textIndex++;
         setTimeout(searchTypeText, 100);
     } else {
         // 타이핑이 끝난 후 다시 시작
-        setTimeout(function() {
+        setTimeout(function () {
             textIndex = 0;
             $(".header-search-input").attr("placeholder", "");
             searchTypeText();
@@ -20,7 +23,22 @@ function searchTypeText() {
     }
 }
 
+function toSearchPage() {
+    const searchQuery = $(".header-search-input").val().trim();
+    if (searchQuery) {
+        // 검색 페이지 이동시 검색어를 파라미터로 전달
+        window.location.href = `../search/search.html?query=${encodeURIComponent(
+            searchQuery
+        )}`;
+    }
+}
+
 $(document).ready(() => {
     searchTypeText();
+    $(".header-search-icon").on("click", toSearchPage);
+    $(".header-search-input").on("keyup", function (e) {
+        if (e.key === "Enter") {
+            toSearchPage();
+        }
+    });
 });
-
